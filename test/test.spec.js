57c31f98d46868230aed54d02dff7d9f84d1c15a
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-04-10 18:20:26 +0200

    initial working state

---
+++ test/test.spec.js
@@ -0,0 +1,12 @@
+import sinon from "sinon";
+import assert from "assert";
+
+describe('Array', function () {
+  const clock = sinon.useFakeTimers();
+
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
